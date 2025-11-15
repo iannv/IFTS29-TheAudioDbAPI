1d653a104cbccc632aa6a6ece783384d6caefc3e
Ian Vázquez <cam.iva@hotmail.com>
2025-11-15 00:52:01 -0300

    inicio

---
+++ TheAudioDbTP/src/app/globalSharedComponents.ts
@@ -0,0 +1,5 @@
+import { Footer } from './shared/footer/footer';
+import { Header } from './shared/header/header';
+import { Navbar } from './shared/navbar/navbar';
+
+export const SHARED_COMPONENTS = [Header, Navbar, Footer];
---
+++ TheAudioDbTP/src/app/pages/artista/artista.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artista',
+  imports: [],
+  templateUrl: './artista.html',
+  styleUrl: './artista.css',
+})
+export class Artista {
+
+}
