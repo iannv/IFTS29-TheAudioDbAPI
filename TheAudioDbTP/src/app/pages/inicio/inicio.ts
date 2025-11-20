558ae555326b07ceedfbec5ead926173270c5684
Ian Vázquez <cam.iva@hotmail.com>
2025-11-20 00:53:47 -0300

    Interfaz y servicio de Tracks

--- TheAudioDbTP/src/app/pages/inicio/inicio.ts
+++ TheAudioDbTP/src/app/pages/inicio/inicio.ts
@@ -1,24 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { ArtistaService } from '../../services/artista';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Artist, ArtistResponse } from '../../interfaces/artist.interface';
+import { trackService } from '../../services/tracks';
+import { Track } from '../../interfaces/track.interface';
 
 @Component({
   selector: 'app-inicio',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './inicio.html',
   styleUrl: './inicio.css',
 })
 export class Inicio implements OnInit {
   constructor(
     private artistaService: ArtistaService,
+    private trackService: trackService,
     private route: ActivatedRoute
   ) {}
 
   public listaArtistas: Artist[] = [];
+  public listaTracks: Track[] = [];
 
   ngOnInit(): void {
     this.obtenerTop5Artistas();
+    this.obtenerTop5Tracks();
   }
 
   public obtenerTop5Artistas() {
@@ -40,6 +45,16 @@ export class Inicio implements OnInit {
 
     this.artistaService.getArtistaTop5().subscribe((artista) => {
       this.listaArtistas.push(artista.artists[0]);
+    });
+  }
+
+  public obtenerTop5Tracks() {
+    this.trackService.getTrackTop1().subscribe((track) => {
+      this.listaTracks.push(track.track[0]);
+    });
+
+    this.trackService.getTrackTop2().subscribe((track) => {
+      this.listaTracks.push(track.track[0]);
     });
   }
 }
