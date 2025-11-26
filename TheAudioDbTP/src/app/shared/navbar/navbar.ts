d65f9ae4c218c9f46bf1c527246ee51976832ecc
Matías Spataro <matiasarielspataro@gmail.com>
2025-11-26 12:04:55 -0300

    selector en navbar de artista/album

--- TheAudioDbTP/src/app/shared/navbar/navbar.ts
+++ TheAudioDbTP/src/app/shared/navbar/navbar.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ArtistaService } from '../../services/artista';
+import { AlbumesService } from '../../services/albumes';
 import {ArtistResponse} from '../../interfaces/artist.interface';
+import { AlbumResponse } from '../../interfaces/album.interface';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -13,17 +15,43 @@ import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 })
 export class Navbar {
   searchTerm: string = '';
+  searchType: 'artist' | 'album' = 'artist';
 
-  constructor(private artistaService: ArtistaService,private router: Router) {}
+  constructor(private artistaService: ArtistaService,
+    private albumesService: AlbumesService,
+    private router: Router) {}
 
   search(): void {
-    console.log("presionado con : " + this.searchTerm)
-    if (this.searchTerm.trim()) {
-      this.artistaService.buscarArtista(this.searchTerm).subscribe((response: ArtistResponse) => {
-        this.router.navigate(['/artista', response.artists[0].idArtist]);
+    if (!this.searchTerm.trim()) {
+      return;
+    }
+
+    if (this.searchType === 'artist') {
+      // *** BÚSQUEDA DE ARTISTA ***
+      this.artistaService.buscarArtista(this.searchTerm).subscribe({
+        next: (response: ArtistResponse) => {
+          if (response.artists && response.artists.length > 0) {
+            this.router.navigate(['/artista', response.artists[0].idArtist]);
+          } else {
+            console.log('No se encontró el artista');
+          }
+        },
+        error: (err) => console.error(err)
       });
+
     } else {
-      console.log('El término de búsqueda está vacío.');
+      // *** BÚSQUEDA DE ÁLBUM ***
+      this.albumesService.buscarAlbumPorNombre(this.searchTerm).subscribe({
+        next: (response: AlbumResponse) => {
+          if (response.album && response.album.length > 0) {
+            const albumEncontrado = response.album[0];
+            this.router.navigate(['/albumes', albumEncontrado.idArtist, albumEncontrado.idAlbum]);
+          } else {
+            console.log('No se encontró el álbum');
+          }
+        },
+        error: (err) => console.error(err)
+      });
     }
   }
 }
